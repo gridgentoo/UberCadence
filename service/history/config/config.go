dd8b29d54c8d4dbd101102be5ac6ac2e2ba4ed1b
Max K <mkol@uber.com>
2020-05-15 14:26:41 -0700

    enabled global cache

--- service/history/config/config.go
+++ service/history/config/config.go
@@ -263,7 +263,7 @@ func New(dc *dynamicconfig.Collection, numberOfShards int, storeType string, isA
 		EventsCacheInitialSize:          dc.GetIntProperty(dynamicconfig.EventsCacheInitialSize, 128),
 		EventsCacheMaxSize:              dc.GetIntProperty(dynamicconfig.EventsCacheMaxSize, 512),
 		EventsCacheTTL:                  dc.GetDurationProperty(dynamicconfig.EventsCacheTTL, time.Hour),
-		EventsCacheGlobalEnable:         dc.GetBoolProperty(dynamicconfig.EventsCacheGlobalEnable, false),
+		EventsCacheGlobalEnable:         dc.GetBoolProperty(dynamicconfig.EventsCacheGlobalEnable, true),
 		// EventsCacheGlobalInitialSize default value 16k shards * 128
 		EventsCacheGlobalInitialSize: dc.GetIntProperty(dynamicconfig.EventsCacheInitialSize, 2048000),
 		// EventsCacheGlobalMaxSize efault value 16k shards * 512
